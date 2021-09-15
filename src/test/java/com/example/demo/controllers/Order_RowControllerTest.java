73833a0484fc316c4d97e4d1df9307fc495ceff7
madeleine.lennhamn <madeleine.lennhamn@gmail.com>
2021-09-15 08:44:40 +0200

    Added tests

--- src/test/java/com/example/demo/controllers/Order_RowControllerTest.java
+++ src/test/java/com/example/demo/controllers/Order_RowControllerTest.java
@@ -1,11 +1,54 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Order_Row;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-   /**
-    * Cretated by Madeleine Lennhamn
-    * Date: 2021-09-13
-    * Time: 20:54
-    * Project: $ {PROJECT_NAME}
-    * Copyright: MIT
-    */
-    class Order_RowControllerTest {
-  
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class Order_RowControllerTest {
+
+    @Test
+    void addOrderRow() {
+        Order_RowController controller = mock(Order_RowController.class);
+
+        String isSavedMessage = controller.AddOrderRow(15L,120L, 1L);
+
+        when(controller.AddOrderRow(15L,120L, 1L)).thenReturn(isSavedMessage);
+
+        assertEquals(controller.AddOrderRow(15L,120L, 1L), isSavedMessage);
+
+        verify(controller, times(2)).AddOrderRow(anyLong(),anyLong(),anyLong());
+
+    }
+
+    @Test
+    void getOrderWithProduct() {
+        Order_RowController controller = mock(Order_RowController.class);
+
+        Iterable<Order_Row> orderWithProduct = controller.getOrderWithProduct(120L);
+
+        when(controller.getOrderWithProduct(120L)).thenReturn(orderWithProduct);
+
+        assertEquals(controller.getOrderWithProduct(120L), orderWithProduct);
+
+        verify(controller, times(2)).getOrderWithProduct(anyLong());
+
+
+    }
+
+    @Test
+    void deleteProduct() {
+        Order_RowController controller = mock(Order_RowController.class);
+
+        String isDeletedMessage = controller.deleteProduct(15L);
+
+        when(controller.deleteProduct(15L)).thenReturn(isDeletedMessage);
+
+        assertEquals(controller.deleteProduct(15L), isDeletedMessage);
+
+        verify(controller, times(2)).deleteProduct(anyLong());
+
+    }
 }
