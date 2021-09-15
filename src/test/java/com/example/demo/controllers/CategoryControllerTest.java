6b4fe4d3acb33edf426aed8cb0995e33044a5add
madeleine.lennhamn <madeleine.lennhamn@gmail.com>
2021-09-15 08:46:04 +0200

    Added tests

--- src/test/java/com/example/demo/controllers/CategoryControllerTest.java
+++ src/test/java/com/example/demo/controllers/CategoryControllerTest.java
@@ -1,11 +1,108 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Category;
+import com.example.demo.repositories.CategoryRepository;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-   /**
-    * Cretated by Madeleine Lennhamn
-    * Date: 2021-09-13
-    * Time: 11:55
-    * Project: $ {PROJECT_NAME}
-    * Copyright: MIT
-    */
-    class CategoryControllerTest {
-  
+import static org.mockito.Mockito.*;
+
+class CategoryControllerTest {
+
+    @Test
+    void addNewCategory() {
+
+        CategoryController controller = mock(CategoryController.class);
+        CategoryRepository categoryRepository = mock(CategoryRepository.class);
+
+        String newCategoryMessage = controller.addNewCategory("Frukt");
+
+        when(controller.addNewCategory("Frukt")).thenReturn(newCategoryMessage);
+
+        assertEquals(controller.addNewCategory("Frukt"),newCategoryMessage);
+
+        verify(controller,times (2)).addNewCategory("Frukt");
+
+    }
+
+    @Test
+    void getCategoryByID() {
+
+        CategoryController controller = mock(CategoryController.class);
+        CategoryRepository categoryRepository = mock(CategoryRepository.class);
+
+        Category category = controller.getCategoryByID(15);
+        Iterable<Category> allCategories = categoryRepository.findAll();
+
+        when(categoryRepository.findAll()).thenReturn(allCategories);
+        when(controller.getCategoryByID(15)).thenReturn(category);
+
+        assertEquals(controller.getCategoryByID(15),category);
+
+        verify(controller, times(2)).getCategoryByID(15);
+        verify(categoryRepository).findAll();
+
+    }
+
+    @Test
+    void updateName() {
+
+        CategoryController controller = mock(CategoryController.class);
+
+        String updateNameMessage = controller.updateName(15, "nyttNamn");
+
+        when(controller.updateName(15, "nyttNamn")).thenReturn(updateNameMessage);
+
+        assertEquals(controller.updateName(15, "nyttNamn"), updateNameMessage);
+
+        verify(controller,times(2)).updateName(anyInt(), any());
+    }
+
+    @Test
+    void deleteCategory() {
+
+        CategoryController controller = mock(CategoryController.class);
+
+        String deleteCategoryMessage = controller.deleteCategory(15);
+
+        when(controller.deleteCategory(15)).thenReturn(deleteCategoryMessage);
+
+        assertEquals(controller.deleteCategory(15),deleteCategoryMessage);
+
+        verify(controller,times(2)).deleteCategory(anyInt());
+    }
+
+    @Test
+    void categoryForProduct() {
+        CategoryRepository categoryRepository = mock(CategoryRepository.class);
+        CategoryController controller = mock(CategoryController.class);
+
+        Iterable<Category> categories = controller.CategoryForProduct(15L);
+
+        when(categoryRepository.findCategoryByProductID(15L)).thenReturn(categories);
+        when(controller.CategoryForProduct(15L)).thenReturn(categories);
+
+        assertEquals(categoryRepository.findCategoryByProductID(15L),categories);
+
+        verify(categoryRepository).findCategoryByProductID(any());
+        verify(controller).CategoryForProduct(any());
+    }
+
+    @Test
+    void remove() {
+        final CategoryController controller = mock(CategoryController.class);
+        final CategoryRepository categoryRepository = mock(CategoryRepository.class);
+        final boolean categoryIsRemoved = controller.remove(15);
+
+        Iterable<Category> allCategories = categoryRepository.findAll();
+
+        when(controller.remove(15)).thenReturn(categoryIsRemoved);
+        when(categoryRepository.findAll()).thenReturn(allCategories);
+
+        assertEquals(controller.remove(15), categoryIsRemoved);
+
+        verify(controller,times(2)).remove(anyInt());
+        verify(categoryRepository).findAll();
+        verify(controller,times(2)).remove(15);
+    }
 }
--- src/test/java/com/example/demo/controllers/CustomerControllerTest.java
+++ src/test/java/com/example/demo/controllers/CustomerControllerTest.java
@@ -1,11 +1,117 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Customer;
+import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.repositories.CustomerRepository;
+import com.example.demo.repositories.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-   /**
-    * Cretated by Madeleine Lennhamn
-    * Date: 2021-09-13
-    * Time: 20:26
-    * Project: $ {PROJECT_NAME}
-    * Copyright: MIT
-    */
-    class CustomerControllerTest {
-  
+import static org.mockito.Mockito.*;
+
+
+class CustomerControllerTest {
+
+    @Autowired
+    ProductRepository productRepositoryRepo;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void addCustomer() {
+        CustomerController controller = mock(CustomerController.class);
+        CustomerRepository customerRepository = mock(CustomerRepository.class);
+
+        String isSavedMessage = controller
+                .addCustomer("Anna", "Sten", "mail@gmail.com","pass", "Gatan 2",
+                                11111L,"Stockholm", "15" );
+
+        when(controller
+                .addCustomer("Anna", "Sten", "mail@gmail.com","pass", "Gatan 2",
+        11111L,"Stockholm", "15")).thenReturn(isSavedMessage);
+
+        verify(controller,times(1))
+                .addCustomer("Anna", "Sten", "mail@gmail.com","pass",
+                        "Gatan 2", 11111L,"Stockholm", "15");
+
+        verify(customerRepository).save(any());
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerController controller = mock(CustomerController.class);
+        CustomerRepository customerRepository = mock(CustomerRepository.class);
+
+        String isUpdatedMessage = controller
+                .updateCustomer("15", "Anna", "Sten", "mail@gmail.com", "Gatan 2",
+                11111L,"Stockholm");
+
+        when(controller
+                .updateCustomer("15", "Anna", "Sten", "mail@gmail.com", "Gatan 2",
+                11111L,"Stockholm")).thenReturn(isUpdatedMessage);
+
+        verify(controller,times(1))
+                .updateCustomer("15","Anna", "Sten", "mail@gmail.com","Gatan 2",
+                        11111L,"Stockholm");
+
+        verify(customerRepository).save(any());
+
+    }
+
+    @Test
+    void deleteCustomer() {
+        final CustomerController controller =  mock(CustomerController.class);
+        CustomerRepository customerRepository = mock(CustomerRepository.class);
+
+        String isDeletedMessage = controller.deleteCustomer(15);
+
+        when(controller.deleteCustomer(15)).thenReturn(isDeletedMessage);
+
+        verify(controller).deleteCustomer(15);
+
+    }
+
+    @Test
+    void updateCustomerPassword() {
+        final CustomerController controller = mock(CustomerController.class);
+        CustomerRepository customerRepository = mock(CustomerRepository.class);
+
+        String isUpdatedMessage = controller.updateCustomerPassword("15", "newPassword");
+
+        when(controller.updateCustomerPassword("15", "newPassword")).thenReturn(isUpdatedMessage);
+
+        assertEquals(controller.updateCustomerPassword("15", "newPassword"), isUpdatedMessage);
+
+        verify(controller,times(2)).updateCustomerPassword(any(),any());
+
+        verify(customerRepository).save(any());
+    }
+
+    @Test
+    void getCustomerById() {
+        final CustomerController controller = mock(CustomerController.class);
+        CustomerRepository customerRepository = mock(CustomerRepository.class);
+
+        Optional<Customer> customerById = controller.getCustomerById(15L);
+
+        when(controller.getCustomerById(15L)).thenReturn(customerById);
+
+        assertEquals(controller.getCustomerById(15L), customerById);
+
+        verify(controller,times(2)).getCustomerById(any());
+        verify(customerRepository).save(any());
+
+
+    }
+
 }
