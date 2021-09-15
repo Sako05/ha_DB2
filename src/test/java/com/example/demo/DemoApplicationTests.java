22cd49978a1a73769ec344c063f7a79fb9d3c3c1
madeleine.lennhamn <madeleine.lennhamn@gmail.com>
2021-09-15 08:46:31 +0200

    Started working on Integrationtest...

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    MockMvc MockMvc;
+
+    private final static WireMockServer wireMockServer = new WireMockServer(1234);
+
+    @BeforeEach
+    public void before() {
+        wireMockServer.start();
     }
+
+    @AfterEach
+    public void after() {
+        wireMockServer.stop();
+    }
+
+
 
 }
